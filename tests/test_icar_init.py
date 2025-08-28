e88a4a9db0e89e778a380ffeae51fead6bea27fe
Jules Robichaud-Gagnon <j.robichaudg@gmail.com>
2025-08-28 11:07:20 -0400

    adapt tests for dev version

--- tests/test_icar_init.py
+++ tests/test_icar_init.py
@@ -16,10 +16,10 @@ class TestIcarInit(unittest.TestCase):
     def test_version_tuple_exists(self):
         self.assertTrue(hasattr(icar, "VERSION"))
         self.assertIsInstance(icar.VERSION, tuple)
-        self.assertEqual(len(icar.VERSION), 3)
+        self.assertGreaterEqual(len(icar.VERSION), 3)
 
     def test_version_format(self):
         version_parts = icar.__version__.split(".")
-        self.assertEqual(len(version_parts), 3)
-        for part in version_parts:
+        self.assertGreaterEqual(len(version_parts), 3)
+        for part in version_parts[:3]:  # Check only major, minor, patch
             self.assertTrue(part.isdigit())
